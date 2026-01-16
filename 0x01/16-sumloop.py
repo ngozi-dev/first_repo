7eb455c2702381000ce6ee17d3728dbac3061aeb
ngozi-dev <franciscaezeaku@gmail.com>
2026-01-16 14:22:35 +0100

    16-sumloop.py

---
+++ 0x01/16-sumloop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# a module that displays addition of numbers from 1 to 10 using loop
+
+total = 0
+for i in range(1, 51):  # 1 to 50 inclusive
+    print(f"{total} + {i}")
+    total += i
+print(total)
+        
+
+
+
+
+
+
+
+
