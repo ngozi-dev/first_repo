af5cb8c723ff9a136ccd60f985b65c8c48ba36b8
ngozi-dev <franciscaezeaku@gmail.com>
2026-01-16 14:11:24 +0100

    1-arithmeticoperators.py

---
+++ 0x00/1-arithmeticoperators.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# a module that demonstrates arithmetic operators in python
+
+a = 10
+b = 3
+print(a + b) # Addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a/b) # Division
+print(a % b) # Modulus
+print(a ** b) # Exponentiation
+print(a // b) # Floor Division
+
+a += b # a = a + b
+print(a)
+a -= b # a = a - b
+print(a)
+a *= b # a = a * b
+print(a)
+a /= b # a = a / b
+print(a)
+a %= b # a = a % b
+print(a)
+a **= b # a = a ** b
+print(a)   
+a //= b # a = a // b
+print(a)    
+x = 10
+x += 5
+print(x)
+print(round (2.9)) # rounds to the nearest integer
+print(abs(-7.5)) # returns the absolute value
+print(pow(2, 3)) # calculates 2 raised to the power of 3        
+
